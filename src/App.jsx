9481cc96ec837b7535de5cafbbd3f4b294094416
z9877969 <z9877969@ukr.net>
2024-03-08 15:25:02 +0200

    adds base structure and linter settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import { Footer } from 'modules/footer';
 import { Header } from 'modules/header';
-import { useEffect, useState } from 'react';
-
-// const b = 65;
-// const a = 65;
-
-// console.log('object');
 
 function App() {
-  const [a] = useState(0);
-  useEffect(() => {
-    // const b = a;
-  }, [a]);
   return (
     <>
       <Header />
