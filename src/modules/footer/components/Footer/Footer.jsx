61cd55184415392f14d40ddfe6921405aa403b12
z9877969 <z9877969@ukr.net>
2024-03-09 15:46:18 +0200

    adds LinkButton component

--- src/modules/footer/components/Footer/Footer.jsx
+++ src/modules/footer/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { Container, Logo, MainTitle } from 'shared/components';
 import s from './Footer.module.scss';
 import FooterNav from '../FooterNav/FooterNav';
+import SocialIcons from '../SocialIcons/SocialIcons';
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
         <Logo />
         <FooterNav />
         <MainTitle title={'Some footer title'} />
-        <h2>SocialIcons</h2>
+        <SocialIcons />
         <h2>SaleForm</h2>
         <h2>FooterInfo</h2>
       </Container>
--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -5,3 +5,4 @@ export { default as Container } from './Container/Container';
 export { default as Button } from './Button/Button';
 export { default as RoundButton } from './RoundButton/RoundButton';
 export { default as ProductCardItem } from './ProductCardItem/ProductCardItem';
+export { default as LInkButton } from './LInkButton/LInkButton';
