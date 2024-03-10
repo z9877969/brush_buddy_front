2b678ef917e39ae4e95ee96266d368149e352f34
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-10 12:58:42 -0500

    AboutBrushBuddyMain (marking done and responsive)

--- src/modules/mainPageAbout/components/MainPageAbout/MainPageAbout.jsx
+++ src/modules/mainPageAbout/components/MainPageAbout/MainPageAbout.jsx
@@ -1,10 +1,78 @@
-import { Container } from 'shared/components';
+import { Container, MainTitle } from 'shared/components';
 import s from './MainPageAbout.module.scss';
+import LinkButton from 'shared/components/LinkButton/LinkButton';
+import PolinaImg from '../../images/PeopleImg/PolinaImg.png';
+import PolinaImg2x from '../../images/PeopleImg/PolinaImg2x.png';
+import petImg from '../../images/petImg/pet.png';
+import petImg2x from '../../images/petImg/petImg2x.png';
+import childImg from '../../images/childImg/child.png';
+import childImg2x from '../../images/childImg/childImg2x.png';
+import toothbrushImg from '../../images/toothbrushImg/toothbrush.png';
+import toothbrushImg2x from '../../images/toothbrushImg/toothbrushImg2x.png';
 
 const MainPageAbout = () => {
   return (
     <section className={s.section}>
-      <Container>MainPageAbout</Container>
+      <Container className={s.containerAbout}>
+        <MainTitle className={s.title} title={'Про БрашБадді'} />
+        <div className={s.container}>
+          <div className={s.paragraphBlock}>
+            <p className={s.text}>
+              BrushBuddy - хаб доглядових товарів за порожниною рота та зубами
+              для дорослих, дітей і твариноk. На чолі з головною прихильницею
+              здорових зубів Поліною, ми зібрали найkраще зубне приладдя на
+              одному сайті.
+            </p>
+            <p className={s.text}>
+              Щиро віримо, що заохочувати до брашингу і ділитися зубними
+              ритуалами kрутіше, ніж ліkувати чи видаляти хворі зуби.
+            </p>
+            <p className={s.text}>
+              В BrushBuddy панує атмосфера здорових та чистих зубів:
+              розповідаємо, яk виkористовувати правила профілаkтиkи вдома та
+              розповсюджуємо тільки яkісні товари.
+            </p>
+          </div>
+          <div className={s.brushBlock}>
+            <div className={s.benefits}>
+              <picture>
+                <source srcSet={`${toothbrushImg} 1x, ${toothbrushImg2x} 2x`} />
+                <img
+                  className={s.benefitsImage}
+                  src={toothbrushImg}
+                  alt="toothbrush"
+                />
+              </picture>
+              <p className={s.benefitsText}>Профілаkтиkа - це kруто</p>
+            </div>
+            <div className={s.benefits}>
+              <picture>
+                <source srcSet={`${petImg} 1x, ${petImg2x} 2x`} />
+                <img className={s.benefitsImage} src={petImg} alt="pet" />
+              </picture>
+              <p className={s.benefitsText}>Щіточkи для твариноk</p>
+            </div>
+            <div className={s.benefits}>
+              <picture>
+                <source srcSet={`${childImg} 1x, ${childImg2x} 2x`} />
+                <img className={s.benefitsImage} src={childImg} alt="child" />
+              </picture>
+              <p className={s.benefitsText}>Кідс&петс френдлі</p>
+            </div>
+          </div>
+          <picture>
+            <source srcSet={`${PolinaImg} 1x, ${PolinaImg2x} 2x`} />
+            <img className={s.image} src={PolinaImg} alt="Polina" />
+          </picture>
+          <div className={s.buttonBlock}>
+            <LinkButton
+              className={s.button}
+              title={'Читати ще'}
+              to={'/AboutPage'}
+            />
+          </div>
+        </div>
+      </Container>
     </section>
   );
 };
