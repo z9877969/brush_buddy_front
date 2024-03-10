4ad82527c3ce55aa62c5959f85a508b0f28434a3
v-duma <139060657+v-duma@users.noreply.github.com>
2024-03-10 20:02:43 +0200

    partitions between the elements of the list of rights have been updated

--- src/modules/footer/components/Rights/Rights.jsx
+++ src/modules/footer/components/Rights/Rights.jsx
@@ -7,9 +7,7 @@ const Rights = () => {
       <nav className={s.rights}>
         <ul className={s.rightsList}>
           <li className={s.rightsListItem}>{'\u00A9'} BrushBuddy 2024</li>
-          <li className={s.verticalLine}>|</li>
           <li className={s.rightsListItem}>Dnipro UA</li>
-          <li className={s.verticalLine}>|</li>
           <li className={s.rightsListItem}>Всі права захищені</li>
         </ul>
       </nav>
