5d5a5eb5d68b943adcdc9ec91149bbca9d7daa45
Andriy Kyrylenko <46637279+z9877969@users.noreply.github.com>
2024-03-11 14:21:18 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistore}>
-        <BrowserRouter>
+        <BrowserRouter basename='/brush_buddy_front/'>
           <App />
         </BrowserRouter>
       </PersistGate>
