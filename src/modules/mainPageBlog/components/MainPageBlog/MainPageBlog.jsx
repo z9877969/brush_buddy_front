1e4084b2cc46f449aef0c08081a32c31d56b08dc
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-03-11 18:46:13 +0200

    add blogId to local state

--- src/modules/mainPageBlog/components/MainPageBlog/MainPageBlog.jsx
+++ src/modules/mainPageBlog/components/MainPageBlog/MainPageBlog.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Container, MainTitle } from 'shared/components';
 import blogData from '../../db/blogData.json';
 import { LinkButton } from 'shared/components';
@@ -5,6 +6,12 @@ import BlogItem from '../BlogItem/BlogItem';
 import s from './MainPageBlog.module.scss';
 
 const MainPaigeBlog = () => {
+  const [blogId, setBlogId] = useState(null);
+
+  useEffect(() => {
+    setBlogId('1');
+  }, []);
+
   const elements = blogData.map((item) => (
     <BlogItem item={item} key={item.id} />
   ));
@@ -16,7 +23,7 @@ const MainPaigeBlog = () => {
         <LinkButton
           title={'Всі статті'}
           className={s.blogBtn}
-          to={'/blog/1}'}
+          to={`/blog/${blogId}`}
         />
       </Container>
     </section>
