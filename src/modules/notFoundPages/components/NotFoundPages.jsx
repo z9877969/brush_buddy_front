dcbbb824fbce8ea4879759d9eab95412a47f88fc
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-11 18:07:14 -0500

    (NotFoundPages) (marking and responcives)

---
+++ src/modules/notFoundPages/components/NotFoundPages.jsx
@@ -0,0 +1,30 @@
+import { Container } from 'shared/components';
+import s from './NotFoundPages.module.scss';
+import { Link } from 'react-router-dom';
+import { sprite } from 'shared/icons';
+import Anima from '../images/Anima.png';
+
+const NotFoundPages = () => {
+  return (
+    <Container>
+      <div className={s.container}>
+        <img className={s.image} src={Anima} alt="not-found-pages" />
+        <h2 className={s.title}> Цієї сторінkи більше немає, але є інші</h2>
+        <div className={s.navigation}>
+          <Link to="/" className={s.link}>
+            На головну
+          </Link>
+          {/* <LinkButton className={s.productLink} title={'До покупок'} /> */}
+          <Link to="/products" className={s.productLink}>
+            До поkупоk
+            <svg>
+              <use href={sprite + '#icon-arrow-right'}></use>
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default NotFoundPages;
