2986201c96c9d15d7167a2ce50fda97eec95333d
Maks <maks135254@gmail.com>
2024-03-12 19:14:18 +0200

    added react select to LangSwitcher and added data for it, changed HeaderNav

--- src/modules/header/components/LangSwitcher/LangSwitcher.jsx
+++ src/modules/header/components/LangSwitcher/LangSwitcher.jsx
@@ -1,11 +1,27 @@
-import s from './LangSwitcher.module.scss';
+import { useEffect, useRef } from 'react';
+import Select from 'react-select';
+import { customStyles } from '../../data/LangSwitcherStyles';
+import { options } from '../../data/optionLang';
 
 const LangSwitcher = () => {
+  const ref = useRef(null);
+
+
+  useEffect(() => {
+    ref.current.inputRef.readOnly = 'true';
+}, [])
+
+  const defaultValue = options.find(option => option.value === "ua");
+
+
   return (
-    <select value={'ua'} className={s.wrapper}>
-      <option value="ua">UA</option>
-      <option value="en">EN</option>
-    </select>
+    <Select
+      ref={ref}
+      options={options}
+      defaultValue={defaultValue}
+      styles={customStyles}
+
+    />
   );
 };
 
