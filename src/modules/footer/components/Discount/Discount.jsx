c8a40f7527887bbc9ed43db6a4f4fcc51ee48075
v-duma <139060657+v-duma@users.noreply.github.com>
2024-03-12 16:32:50 +0200

    the functionality of the footer is done

--- src/modules/footer/components/Discount/Discount.jsx
+++ src/modules/footer/components/Discount/Discount.jsx
@@ -1,7 +1,72 @@
+import { useState, useEffect } from 'react';
 import s from './Discount.module.scss';
 import { Field, Form, Formik } from 'formik';
 
 const Discount = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [discountApplied, setDiscountApplied] = useState(false);
+  const [phoneNumbers, setPhoneNumbers] = useState([]);
+  const [formattedPhoneNumber, setFormattedPhoneNumber] = useState('');
+  const [submitClicked, setSubmitClicked] = useState(false);
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+
+  useEffect(() => {
+    const storedNumbers = localStorage.getItem('phoneNumbers');
+    if (storedNumbers) {
+      setPhoneNumbers(JSON.parse(storedNumbers));
+    }
+  }, []);
+
+  const formatPhoneNumber = (value) => {
+    const cleaned = ('' + value).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{0,3})(\d{0,2})(\d{0,3})$/);
+    if (match) {
+      return (
+        '+38 0' + (match[2] ? +match[2] : '') + (match[3] ? ' ' + match[3] : '')
+      );
+    }
+    return value;
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    const { value } = e.target;
+    setPhoneNumber(value);
+    setFormattedPhoneNumber(formatPhoneNumber(value));
+  };
+
+  const handlePhoneNumberFocus = () => {
+    if (!formattedPhoneNumber) {
+      setFormattedPhoneNumber('+38 0');
+    }
+  };
+
+  const handleCheckboxChange = (e) => {
+    setCheckboxChecked(e.target.checked);
+  };
+
+  const handleFormSubmit = () => {
+    setSubmitClicked(true);
+    if (!checkboxChecked) {
+      return;
+    }
+
+    if (phoneNumbers.includes(phoneNumber)) {
+      setDiscountApplied(true);
+    } else {
+      const updatedNumbers = [...phoneNumbers, phoneNumber];
+      setPhoneNumbers(updatedNumbers);
+      localStorage.setItem('phoneNumbers', JSON.stringify(updatedNumbers));
+      setPhoneNumber('');
+      setFormattedPhoneNumber('');
+      setDiscountApplied(false);
+      // console.log('Масив номерів:', updatedNumbers);
+    }
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+  };
+
   return (
     <div className={s.discountDiv}>
       <h2 className={s.discountTitle}>
@@ -9,25 +74,54 @@ const Discount = () => {
         сховалася тут
       </h2>
 
-      <Formik>
+      <Formik initialValues={{ number: '' }} onSubmit={handleSubmit}>
         <Form className={s.form}>
-          <Field
-            className={s.numberField}
-            placeholder="Номер телефону"
-            name="number"
-          />
-          <label htmlFor="checkboxField" className={s.checkboxFieldLabel}>
-            <input
-              type="checkbox"
-              className={s.checkboxField}
-              id="checkboxField"
+          <div className={s.numberFieldDiv}>
+            <Field
+              type="tel"
+              className={s.numberField}
+              placeholder="Номер телефону"
+              name="number"
+              value={formattedPhoneNumber}
+              onChange={handlePhoneNumberChange}
+              onFocus={handlePhoneNumberFocus}
+              id="numberField"
+              maxLength="15"
             />
-            <p>
-              Я погоджуюсь з умовами обробки{' '}
-              <span className={s.span}>персональних даних</span>
-            </p>
-          </label>
-          <button className={s.btn} type="submit">
+            <label className={s.numberFieldLabel} htmlFor="numberField">
+              Номер телефону
+            </label>
+
+            {discountApplied && (
+              <p className={s.discountAppliedText}>
+                На цей номер знижка вже використана
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="checkboxField"
+              className={s.checkboxFieldLabel}
+              required
+            >
+              <Field
+                type="checkbox"
+                className={s.checkboxField}
+                id="checkboxField"
+                name="checkboxField"
+                checked={checkboxChecked}
+                onChange={handleCheckboxChange}
+              />
+              <p>
+                Я погоджуюсь з умовами обробки{' '}
+                <span className={s.span}>персональних даних</span>
+              </p>
+            </label>
+            {submitClicked && !checkboxChecked && (
+              <p className={s.discountAppliedText}>Доступ обмежено без згоди</p>
+            )}
+          </div>
+          <button className={s.btn} type="submit" onClick={handleFormSubmit}>
             Отримати
           </button>
         </Form>
