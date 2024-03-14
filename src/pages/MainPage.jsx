87f5ae3e84cbe6255b23169261546005f64022b3
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-03-14 17:15:48 +0200

    add module MainPageToShopping, modified component RoundButton

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import { MainPageAbout } from 'modules/mainPageAbout';
 import { MainPageHero } from 'modules/mainPageHero';
 import { MainPageBlog } from 'modules/mainPageBlog';
+import { MainPageToShopping } from 'modules/mainPageToShopping';
 
 const MainPage = () => {
   return (
     <>
       <MainPageHero />
       <MainPageAbout />
+      <MainPageToShopping />
       <MainPageBlog />
     </>
   );
--- src/shared/components/RoundButton/RoundButton.jsx
+++ src/shared/components/RoundButton/RoundButton.jsx
@@ -1,9 +1,19 @@
 import clsx from 'clsx';
 import { sprite } from 'shared/icons';
 
-const RoundButton = ({ iconId, title, className }) => {
+const RoundButton = ({
+  iconId,
+  title,
+  className,
+  onClick = null,
+  disabled = false,
+}) => {
   return (
-    <button className={clsx('button', className && className)}>
+    <button
+      className={clsx('button', className && className)}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {iconId ? (
         <svg>
           <use href={sprite + '#' + iconId}></use>
