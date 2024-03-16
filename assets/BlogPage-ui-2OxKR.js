2908da71ba1aaadd7be7e8cce0111117b9d7d82a
z9877969 <z9877969@ukr.net>
2024-03-16 12:17:45 +0200

    Updates

---
+++ assets/BlogPage-ui-2OxKR.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BfGnCNUG.js";const r=()=>e.jsx("h1",{children:"BlogPage"});export{r as default};
--- assets/ProductCardPage-BNUhrvFB.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-WDHc_t2f.js";const a=()=>r.jsx("h1",{children:"ProductCartPage"});export{a as default};
