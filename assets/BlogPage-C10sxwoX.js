f43621bafdb3f4c68ee9418c4eab0c03e68acb52
z9877969 <z9877969@ukr.net>
2024-03-17 21:42:46 +0200

    Updates

---
+++ assets/BlogPage-C10sxwoX.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DZWGXAv4.js";const r=()=>e.jsx("h1",{children:"BlogPage"});export{r as default};
---
+++ assets/ThankPage-DW3VvknJ.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-DZWGXAv4.js";const r=()=>a.jsx("h1",{children:"ThankPage"});export{r as default};
