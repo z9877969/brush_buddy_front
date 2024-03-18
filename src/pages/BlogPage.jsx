d3eb576b74b56846d8d06bf6e2f155649ada37b0
z9877969 <z9877969@ukr.net>
2024-03-18 15:09:44 +0200

    adds container to BlogView

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,12 @@
-import BlogView from '../modules/blogView/components/BlogView/BlogView';
+import { BlogView } from 'modules/blogView';
+import { Container } from 'shared/components';
 
 const BlogPage = () => {
-  return <BlogView />;
+  return (
+    <Container>
+      <BlogView />
+    </Container>
+  );
 };
 
 export default BlogPage;
