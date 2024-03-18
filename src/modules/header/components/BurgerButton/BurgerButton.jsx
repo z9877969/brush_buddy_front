10081c8908b5402f0584e5dc71e1ea5156b6c0db
Maks <maks135254@gmail.com>
2024-03-18 23:12:03 +0200

    fixed bug of  Mobile menu

--- src/modules/header/components/BurgerButton/BurgerButton.jsx
+++ src/modules/header/components/BurgerButton/BurgerButton.jsx
@@ -1,14 +1,25 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { sprite } from 'shared/icons';
 import s from './BurgerButton.module.scss';
 
 const BurgerButton = ({ onClick, isOpen }) => {
+  const location = useLocation();
+  useEffect(() => {
+    if (isOpen) {
+      onClick(false);
+    }
+    // eslint-disable-next-line
+  }, [location.pathname]);
   return (
     <div>
       <button className={s.btnBurgerMenu} onClick={onClick}>
         <svg width="24" height="24">
-          <use
-            href={`${sprite}#${isOpen ? 'icon-close-cross' : 'icon-burger'}`}
-          ></use>
+          {isOpen ? (
+            <use href={`${sprite}#icon-close-cross`}></use>
+          ) : (
+            <use href={`${sprite}#icon-burger`}></use>
+          )}
         </svg>
       </button>
     </div>
