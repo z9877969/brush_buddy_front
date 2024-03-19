daa8ecde5079e093f8f0313b6118d75d6c22eb94
RayChik <127132649+RayChikS@users.noreply.github.com>
2024-03-19 11:46:17 +0200

    jsx-added

---
+++ src/modules/orderFinished/components/OrderFinished/OrderFinished.jsx
@@ -0,0 +1,25 @@
+import { Container, Logo } from 'shared/components';
+import css from './OrderFinished.scss';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const OrderFinished = () => {
+  const [order] = useState({ number: 375 });
+
+  return (
+    <Container>
+      <Logo className={css.logo} />
+      <h2 className={css.title}>Подякували!</h2>
+      <p className={css.text}>
+        Замовлення №{order.number} успішно оформлене. Ми відправили усю
+        інформацію на вказану електронну адресу. Наші співробітники вже дбайливо
+        збирають посилочку для Вас.
+      </p>
+      <Link className={css.linkButton} to="#">
+        І вам дякуємо
+      </Link>
+    </Container>
+  );
+};
+
+export default OrderFinished;
