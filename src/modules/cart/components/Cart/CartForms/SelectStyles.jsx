4f9dfe84728ffbc5c84b4d758dbe50b64f5e66bc
GermanSTr <gershev1994@gmail.com>
2024-03-19 15:32:37 +0200

    select

--- src/modules/cart/components/Cart/CartForms/SelectStyles.jsx
+++ src/modules/cart/components/Cart/CartForms/SelectStyles.jsx
@@ -1,23 +1,93 @@
 export const selectStyles = {
-  control: (styles) => ({
+  container: (styles) => ({
     ...styles,
-    borderRadius: 16,
-    padding: 12,
-    borderColor: '#1F1F1F33',
-    '&:focus': {
-      borderColor: '#F5516F',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    height: '48px',
+    padding: '12px',
+    alignItems: 'center',
+    borderRadius: '16px',
+    border: '1px solid rgba(31, 31, 31, 0.2)',
+
+    '@media (min-width: 1440px)': {
+      backgroundColor: '#fff',
+      border: 'none',
     },
   }),
-  valueContainer2: (styles) => ({ ...styles, padding: 0 }),
-  input2: (styles) => ({ ...styles, margin: 0, height: 24 }),
+  control: (styles) => ({
+    ...styles,
+    backgroundColor: 'transparent',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    outline: 'none',
+    border: 'none',
+
+    // borderColor: isSelected ? 'red' : 'none',
+    '&:hover': {
+      border: 'none',
+    },
+    '&:focus': {
+      border: 'none',
+    },
+  }),
+  // : (styles { isFocused }) => ({
+  //   ...styles,
+  //   borderColor: isFocused ?"red":"green"
+  // }),
+  valueContainer: (styles) => ({
+    ...styles,
+    padding: 0,
+  }),
+  input: (styles) => ({
+    ...styles,
+    padding: 0,
+  }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    color: '#797979',
+    padding: 0,
+  }),
   indicatorSeparator: (styles) => ({
     ...styles,
     display: 'none',
   }),
-  singleValue: (styles) => ({
+  // placeholder: (styles) => ({
+  //   ...styles,
+  //   fontSize: '16px',
+  //   lineHeight: '24px',
+  //   color: '#a5a5a5',
+  // }),
+  // menu: (styles) => ({
+  //   ...styles,
+  //   backgroundColor: 'red',
+  // }),
+  option: (styles) => ({
     ...styles,
-    color: '#A5A5A5',
-    fontSize: 16,
+    padding: 'none',
+    fontSize: '16px',
+    lineHeight: '24px',
+    color: '#797979',
+    backgroundColor: 'transparent',
+    '&:hover': {
+      color: '#1f1f1f',
+    },
+    '&:focus': {
+      color: '#1f1f1f',
+    },
   }),
-  indicatorsContainer2: (provided) => ({ ...provided, padding: 1 }),
+  menuList: (styles) => ({
+    ...styles,
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: '16px',
+    padding: '12px',
+    backgroundColor: '#fff',
+    gap: '8px',
+    '@media (min-width: 1440px)': {
+      backgroundColor: '#fff',
+      border: 'none',
+    },
+  }),
 };
