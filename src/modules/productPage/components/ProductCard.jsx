4e086c96e2e07afed5778ba9b8e7deb8bf5d96eb
z9877969 <z9877969@ukr.net>
2024-03-19 14:09:43 +0200

    adds product to products list template

--- src/modules/productPage/components/ProductCard.jsx
+++ src/modules/productPage/components/ProductCard.jsx
@@ -12,88 +12,84 @@ import { sprite } from 'shared/icons';
 const ProductCard = () => {
   // const productId = useParams();
   const [quantity, setQuantity] = useState(1);
+
   return (
     <Container>
-      <h2>Product card</h2>
-      <div>
-        <ul>
-          {productCard.map((product) => {
-            return (
-              <li className={s.containerList} key={product.id}>
-                <MyImage imges={product.imgs} />
-                <div className={s.informationBlock}>
-                  <h2 className={s.title}>{product.title}</h2>
-                  <p className={s.paragraph}>{product.text}</p>
-                  {product.salePrice.length > 0 ? (
-                    <div className={s.saleBlock}>
-                      <p className={s.salePrice}>{product.salePrice}</p>
-                      <p className={s.notSalePrice}>{product.price}</p>
-                    </div>
-                  ) : (
-                    <p className={s.price}>{product.price}</p>
-                  )}
-                  {/* <p>Виберіть колір:</p> */}
-                  {/* <p>Виберіть колір: {product.colors.map((color) => {
+      <ul>
+        {[productCard[0]].map((product) => {
+          return (
+            <li className={s.containerList} key={product.id}>
+              <MyImage imges={product.imgs} />
+              <div className={s.informationBlock}>
+                <h2 className={s.title}>{product.title}</h2>
+                <p className={s.paragraph}>{product.text}</p>
+                {product.salePrice.length > 0 ? (
+                  <div className={s.saleBlock}>
+                    <p className={s.salePrice}>{product.salePrice}</p>
+                    <p className={s.notSalePrice}>{product.price}</p>
+                  </div>
+                ) : (
+                  <p className={s.price}>{product.price}</p>
+                )}
+                {/* <p>Виберіть колір:</p> */}
+                {/* <p>Виберіть колір: {product.colors.map((color) => {
                               return <p>{color}</p>
                           })}</p> */}
-                  {product.litter ? (
-                    <Flavor products={product} arr={productCard} />
-                  ) : null}
-                  {product.litter ? (
-                    <Color products={product} arr={productCard} />
-                  ) : null}
-                  {product.litter ? (
-                    <Volume products={product} arr={productCard} />
-                  ) : null}
-                  <p className={s.itemHave}>
-                    {product.itme ? 'Є в наявності' : 'немає в наявності'}
-                  </p>
-                  {/* <Counter
+                {product.litter ? (
+                  <Flavor products={product} arr={productCard} />
+                ) : null}
+                {product.litter ? (
+                  <Color products={product} arr={productCard} />
+                ) : null}
+                {product.litter ? (
+                  <Volume products={product} arr={productCard} />
+                ) : null}
+                <p className={s.itemHave}>
+                  {product.itme ? 'Є в наявності' : 'немає в наявності'}
+                </p>
+                {/* <Counter
                     value={Number(product.quantit)}
                     changeCount={Number(product.quantit)}
                     disabledIncrem={Number(product.quantity)}
                   /> */}
-                  <div className={s.buttonBlock}>
-                    <button
-                      onClick={() => {
-                        setQuantity((product.quantit = quantity - 1));
-                      }}
-                      disabled={product.quantit <= 1}
-                      type="button"
-                    >
-                      <svg width="16" height="16">
-                        <use href={sprite + '#icon-minus'}></use>
-                      </svg>
-                    </button>
-                    <span className={s.quantityText}>{product.quantit}</span>
-                    <button
-                      onClick={() => {
-                        setQuantity((product.quantit = quantity + 1));
-                      }}
-                      disabled={product.quantit >= product.quantity}
-                      type="button"
-                    >
-                      <svg width="16" height="16">
-                        <use href={sprite + '#icon-plus'}></use>
-                      </svg>
-                    </button>
-                  </div>
-                  <div>
-                    <p className={s.description}>Опис</p>
-                    <p className={s.info}>{product.subtitle}</p>
-                  </div>
-                  <div>
-                    <p className={s.recomendation}>Рекомендація</p>
-                    <p className={s.recomendationInfo}>
-                      {product.recomendation}
-                    </p>
-                  </div>
+                <div className={s.buttonBlock}>
+                  <button
+                    onClick={() => {
+                      setQuantity((product.quantit = quantity - 1));
+                    }}
+                    disabled={product.quantit <= 1}
+                    type="button"
+                  >
+                    <svg width="16" height="16">
+                      <use href={sprite + '#icon-minus'}></use>
+                    </svg>
+                  </button>
+                  <span className={s.quantityText}>{product.quantit}</span>
+                  <button
+                    onClick={() => {
+                      setQuantity((product.quantit = quantity + 1));
+                    }}
+                    disabled={product.quantit >= product.quantity}
+                    type="button"
+                  >
+                    <svg width="16" height="16">
+                      <use href={sprite + '#icon-plus'}></use>
+                    </svg>
+                  </button>
                 </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                <div>
+                  <p className={s.description}>Опис</p>
+                  <p className={s.info}>{product.subtitle}</p>
+                </div>
+                <div>
+                  <p className={s.recomendation}>Рекомендація</p>
+                  <p className={s.recomendationInfo}>{product.recomendation}</p>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </Container>
   );
 };
