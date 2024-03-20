f2e8ad86e443569c81f8ce9ff3f816f7f8142e93
RayChikS <vatoresp@gmail.com>
2024-03-20 15:41:44 +0200

    finall-fixes

--- src/modules/cartEmpty/components/CartEmpty/CartEmpty.jsx
+++ src/modules/cartEmpty/components/CartEmpty/CartEmpty.jsx
@@ -1,28 +1,12 @@
 import { Container, LinkButton } from 'shared/components';
 import s from './CartEmpty.module.scss';
 import { ROUTES } from 'shared/constants';
-import { Picture } from 'shared/components';
-
-//img import
-import mob1x from 'modules/cartEmpty/images/empty1_1x.jpg';
-import mob2x from 'modules/cartEmpty/images/empty1_2x.jpg';
-import tabDeck1x from 'modules/cartEmpty/images/empty2_1x.jpg';
-import tabDeck2x from 'modules/cartEmpty/images/empty2_2x.jpg';
+import { animation } from 'modules/cartEmpty/images';
 
 const CartEmpty = () => {
   return (
     <Container className={s.section}>
-      <Picture
-        mob1x={mob1x}
-        mob2x={mob2x}
-        tab1x={tabDeck1x}
-        tab2x={tabDeck2x}
-        desk1x={tabDeck1x}
-        desk2x={tabDeck2x}
-        defaultImg={mob1x}
-        width={343}
-        alt="photo"
-      />
+      <img src={animation} alt="animation" className={s.animation} />
 
       <h2 className={s.title}>Кошик порожній</h2>
 
--- src/pages/ThankPage.jsx
+++ src/pages/ThankPage.jsx
@@ -1,10 +1,7 @@
-import { CartEmpty } from 'modules/cartEmpty';
-
 const ThankPage = () => {
   return (
     <>
       <h1>ThankPage</h1>
-      <CartEmpty />
     </>
   );
 };
