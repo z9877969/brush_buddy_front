ca030da9283a9333c074f0f8587160ec4368f3ae
Liubov <drogaltseva92@gmail.com>
2024-03-21 14:34:08 +0100

    create custom hook for addProduct action, fix layout in the cart

--- src/modules/cart/components/CarListCurrentItem/CartListCurrentItem.jsx
+++ src/modules/cart/components/CarListCurrentItem/CartListCurrentItem.jsx
@@ -11,77 +11,102 @@ const CartListCurrentItem = ({
   const elements = data.map(
     ({
       id,
-      image,
-      quantity,
-      name,
-      color,
+      images,
+      title,
+      colors,
       price,
-      discounted_price,
-      flavor,
+      salePrice,
+      flavors,
       volume,
       isDisabledIncrement,
-    }) => (
-      <li className={s.itemproduct} key={id} id={id}>
-        <img className={s.itemImg} src={image} alt={name} />
-        <div className={s.itemInfo}>
-          <div>
-            <p className={s.itemName}>{name}</p>
-            <div className={s.itemDetails}>
-              {flavor && <p className={s.itemFlavor}>Смак: {flavor}</p>}
-              {volume && <p className={s.itemVol}>об’єм: {volume}</p>}
-              {color && <p className={s.itemColor}>Колір: {color}</p>}
+      amount = 1,
+    }) => {
+      return (
+        <li className={s.itemproduct} key={id} id={id}>
+          <img className={s.itemImg} src={images.url} alt={title} />
+          <div className={s.itemInfo}>
+            <div>
+              <p className={s.itemName}>{title}</p>
+              <div className={s.itemDetails}>
+                {flavors && flavors.length > 0 && (
+                  <div className={s.itemFlavor}>
+                    Смак:
+                    {flavors.map((flavor, index) => (
+                      <span key={index}>
+                        {flavor.flavor}
+                        {index !== flavors.length - 1 && ', '}
+                      </span>
+                    ))}
+                  </div>
+                )}
+                {colors && colors.name && (
+                  <p className={s.itemColor}>Колір: {colors.name}</p>
+                )}
+                {volume && <p className={s.itemVol}>об’єм: {volume}</p>}
+              </div>
+            </div>
+            <div className={s.prices}>
+              {salePrice !== 0 && (
+                <p className={s.itemDiscPrice}>{salePrice} грн</p>
+              )}
+              {salePrice !== 0 ? (
+                <p className={s.itemPriceDisc}>{price} грн</p>
+              ) : (
+                <p className={s.itemPrice}>{price} грн</p>
+              )}
+            </div>
+            <div className={s.itemFooter}>
+              <Counter
+                classWrapper={s.counter}
+                classSvg={s.classSvg}
+                value={amount}
+                changeCount={(newCount) =>
+                  changeCount({
+                    id,
+                    flavor: flavors[0]?.flavor,
+                    quantity: flavors[0]?.quantity || colors.quantity,
+                    volume: flavors[0]?.volume,
+                    color: colors?.color,
+                    newCount,
+                  })
+                }
+                disabledIncrem={isDisabledIncrement}
+              />
+              <button
+                type="button"
+                className={s.deleteBtn}
+                onClick={() => onClickDelete(id)}
+              >
+                <svg>
+                  <use href={sprite + '#icon-delete'}></use>
+                </svg>
+              </button>
+              {/* <button
+                className={s.btnTest}
+                type="submit"
+                onClick={() =>
+                  onClickAdd({
+                    id,
+                    images,
+                    amount,
+                    title,
+                    colors,
+                    price,
+                    salePrice,
+                    flavors,
+                    volume,
+                  })
+                }
+              >
+                Test btn
+              </button> */}
             </div>
           </div>
-          <div className={s.prices}>
-            {discounted_price && (
-              <p className={s.itemDiscPrice}>{discounted_price} грн</p>
-            )}
-            {discounted_price ? (
-              <p className={s.itemPriceDisc}>{price} грн</p>
-            ) : (
-              <p className={s.itemPrice}>{price} грн</p>
-            )}
-          </div>
-          <div className={s.itemFooter}>
-            <Counter
-              classWrapper={s.counter}
-              classSvg={s.classSvg}
-              value={quantity}
-              changeCount={(newCount) => changeCount({ id, newCount })}
-              disabledIncrem={isDisabledIncrement}
-            />
-            <button
-              type="button"
-              className={s.deleteBtn}
-              onClick={() => onClickDelete(id)}
-            >
-              <svg>
-                <use href={sprite + '#icon-delete'}></use>
-              </svg>
-            </button>
-            {/* <button className={s.btnTest}
-              type="submit"
-              onClick={() =>
-                onClickAdd({
-                  id,
-                  image,
-                  quantity,
-                  name,
-                  color,
-                  price,
-                  discounted_price,
-                  flavor,
-                  volume,
-                })
-              }
-            >
-              Test btn
-            </button> */}
-          </div>
-        </div>
-      </li>
-    )
+        </li>
+      );
+    }
   );
   return elements;
 };
+
 export default CartListCurrentItem;
