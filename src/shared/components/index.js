1893a2244b3128425aa86722eac65ab581b54455
z9877969 <z9877969@ukr.net>
2024-03-22 21:46:24 +0200

    adds toastify and moves card item components from inside to outside

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -8,5 +8,6 @@ export { default as ProductCardItem } from './ProductCardItem/ProductCardItem';
 export { default as LinkButton } from './LinkButton/LinkButton';
 export { default as Loader } from './Loader/Loader';
 export { default as Picture } from './Picture/Picture';
-export { default as ProductTypeIcon } from './ProductCardItem/ProductTypeIcon/ProductTypeIcon';
-export { default as ProductWatermark } from './ProductCardItem/ProductWatermark/ProductWatermark';
+export { default as ProductTypeIcon } from './ProductTypeIcon/ProductTypeIcon';
+export { default as ProductWatermark } from './ProductWatermark/ProductWatermark';
+export { default as Toastify } from './Toastify/Toastify';
