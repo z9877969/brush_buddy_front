f19a77e0ea89b4ee0ad1b3b4bdecebe11691dbb2
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-26 23:12:59 -0500

    (FixProductCardPage) fixed

--- src/modules/productPage/components/ProductCard.jsx
+++ src/modules/productPage/components/ProductCard.jsx
@@ -283,37 +283,46 @@ const ProductCard = () => {
                   //   s.btnDontWorking && s.btnTest
                   // )}
                   type="submit"
-                  onClick={
-                    () =>
-                      onClickAdd({
-                        id: product.id,
-                        imgUrl: product.images[0],
-                        price: product.price,
-                        salePrice: product.salePrice,
-                        title: product.title,
-                        volume: product.volume.length === 0 ? null : mls,
-                        flavor: product.flavors.length === 0 ? null : flavor,
-                        color: product.colors.length === 0 ? null : color,
-                        amount: quantity,
-                        quantity:
-                          product.flavors[0]?.quantity ??
-                          product.colors[0]?.quantity,
-                      })
-                    // console.log({
-                    //   id: product.id,
-                    //   imgUrl: product.images[0],
-                    //   price: product.price,
-                    //   salePrice: product.salePrice,
-                    //   title: product.title,
-                    //   volume: product.volume.length === 0 ? null : mls,
-                    //   flavor: product.flavors.length === 0 ? null : flavor,
-                    //   color: product.colors.length === 0 ? null : color,
-                    //   amount: quantity,
-                    //   quantity:
-                    //     product.flavors[0]?.quantity ??
-                    //     product.colors[0]?.quantity,
-                    // })
-                  }
+                  onClick={() => {
+                    //  return onClickAdd({
+                    //     id: product.id,
+                    //     title: product.title,
+                    //     price: product.price,
+                    //     images: product.images[0],
+                    //     quantity: quantity,
+                    //   });
+                    onClickAdd({
+                      id: product.id,
+                      images: product.images[0].url,
+                      price: product.price,
+                      salePrice: product.salePrice,
+                      title: product.title,
+                      volume: mls?.volume ?? '',
+                      flavor: flavor?.flavor ?? '',
+                      color: color?.color ?? '',
+                      amount: quantity,
+                      quantity:
+                        product.flavors[0]?.quantity ??
+                        product.colors[0]?.quantity,
+                      name: mls?.name ?? flavor?.name ?? color?.name,
+                    });
+                    console.log({
+                      id: product.id,
+                      imgUrl: product.images[0].url,
+                      price: product.price,
+                      salePrice: product.salePrice,
+                      title: product.title,
+                      volume: product.volume.length === 0 ? '' : mls.volume,
+                      flavor:
+                        product.flavors.length === 0 ? '' : flavor.colorMarker,
+                      color: product.colors.length === 0 ? '' : color.color,
+                      amount: quantity,
+                      quantity:
+                        product.flavors[0]?.quantity ??
+                        product.colors[0]?.quantity,
+                      name: mls?.name ?? flavor?.name ?? color?.name,
+                    });
+                  }}
                   disabled={flavor?.quantity === 0 && color?.quantity === 0}
                 >
                   В кошик
