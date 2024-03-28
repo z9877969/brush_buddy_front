7ff0807558193e035feb728c3f2f362b3ddd2913
Liubov <drogaltseva92@gmail.com>
2024-03-28 23:39:36 +0100

    fix cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,18 +1,22 @@
 import { Cart } from 'modules/cart';
-import { selectProd } from '@redux/cart/selectorsCart';
+import { selectProd, selectSubmitForm } from '@redux/cart/selectorsCart';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const CartPage = () => {
   const products = useSelector(selectProd);
+  const isSubmit = useSelector(selectSubmitForm);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (products.length === 0) {
       navigate('/cart-empty', { replace: true });
     }
-  }, [products, navigate]);
+    if (isSubmit) {
+      navigate('/thank', { replace: true });
+    }
+  }, [products, navigate, isSubmit]);
 
   return <>{products.length > 0 && <Cart />}</>;
 };
