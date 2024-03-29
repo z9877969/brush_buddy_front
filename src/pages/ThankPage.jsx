3cea303ddc2a9837e62a5f3c88a968ecef1f1afa
Liubov <drogaltseva92@gmail.com>
2024-03-29 17:03:10 +0100

    fix location

--- src/pages/ThankPage.jsx
+++ src/pages/ThankPage.jsx
@@ -1,12 +1,18 @@
+import { submitForm } from '@redux/cart/cartSlice';
 import { selectOrderNum } from '@redux/order/orderSelectors';
 import { resetOrderNum } from '@redux/order/orderSlice';
 import { OrderFinished } from 'modules/orderFinished';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const ThankPage = () => {
   const dispatch = useDispatch();
   const orderNum = useSelector(selectOrderNum);
+  const location = useLocation();
+  if (location.pathname === '/thank') {
+    dispatch(submitForm(false));
+  }
 
   useEffect(() => {
     return () => {
