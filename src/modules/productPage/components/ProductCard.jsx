129ca9ad92f6c2b82a51ac5f993eaad9c5baee04
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-29 10:59:03 -0500

    (FixProductCardPage) fix

--- src/modules/productPage/components/ProductCard.jsx
+++ src/modules/productPage/components/ProductCard.jsx
@@ -143,7 +143,9 @@ const ProductCard = ({ product }) => {
                 setFlavor={setFlavor}
               />
             ) : null}
-            {product.colors?.length > 0 && color ? (
+            {product.colors?.length > 0 &&
+            product.colors[0].color.length > 0 &&
+            color ? (
               <Color
                 productColors={product.colors}
                 value={color}
