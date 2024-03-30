dd16c94c1aff11d3e7350838d046032cac67807f
z9877969 <z9877969@ukr.net>
2024-03-30 02:24:02 +0200

    fixes filters displaying

--- src/modules/selectedFilters/components/FilterItem/FilterItem.jsx
+++ src/modules/selectedFilters/components/FilterItem/FilterItem.jsx
@@ -1,42 +1,21 @@
-import { RoundButton } from 'shared/components';
-import { PRODUCT_TYPES } from 'shared/constants';
 import clsx from 'clsx';
+import { RoundButton } from 'shared/components';
 import s from './FilterItem.module.scss';
 
-const FilterItem = ({ filterName, removeFilter }) => {
-  let nameToShow = null;
-  let subClass = null;
-
-  if (
-    filterName === PRODUCT_TYPES.ADULT ||
-    filterName === PRODUCT_TYPES.ANIMAL ||
-    filterName === PRODUCT_TYPES.CHILD
-  ) {
-    const filterNames = {
-      child: 'Для дітей',
-      adult: 'Для дорослих',
-      animal: 'Для тварин',
-    };
-    nameToShow = filterNames[filterName];
-    subClass = filterName;
-  }
-
-  const isVisible = filterName && filterName !== 'search' && filterName.search;
-
+const FilterItem = ({ filterName, removeFilter, subClass }) => {
   return (
-    isVisible && (
-      <li>
-        <div className={clsx(s.filterBlock, subClass && s[subClass])}>
-          {nameToShow ? nameToShow : filterName}
-          <RoundButton
-            iconId={'icon-close-cross'}
-            className={s.deleteBtn}
-            onClick={() => removeFilter(filterName)}
-          />
-        </div>
-      </li>
-    )
+    <li>
+      <div className={clsx(s.filterBlock, subClass && s[subClass])}>
+        {filterName}
+        <RoundButton
+          iconId={'icon-close-cross'}
+          className={s.deleteBtn}
+          onClick={() => removeFilter(filterName)}
+        />
+      </div>
+    </li>
   );
+  // );
 };
 
 export default FilterItem;
