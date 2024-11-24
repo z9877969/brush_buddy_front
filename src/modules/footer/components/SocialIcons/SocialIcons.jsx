0388ea8a42381604cd7eee58bfb0a8e01c57efec
z9877969 <z9877969@ukr.net>
2024-11-24 15:47:28 +0200

    adds api to get social links

--- src/modules/footer/components/SocialIcons/SocialIcons.jsx
+++ src/modules/footer/components/SocialIcons/SocialIcons.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { SOCIAL_NETWORKS } from 'shared/constants';
+// import { SOCIAL_NETWORKS } from 'shared/constants';
 import { sprite } from 'shared/icons';
 import { getSocialLinksApi } from 'services/brushbuddyApi';
 import s from './SocialIcons.module.scss';
@@ -18,22 +18,22 @@ const SocialIcons = () => {
 
   return (
     <div className={s.iconsFooter}>
-      <a href={SOCIAL_NETWORKS.INSTAGRAM} target="_blank">
+      <a href={socialLinks.INSTAGRAM} target="_blank">
         <svg id="icon-instagram" className={s.iconSocial}>
           <use href={sprite + '#icon-instagram'}></use>
         </svg>
       </a>
-      <a href={SOCIAL_NETWORKS.TELEGRAM} target="_blank">
+      <a href={socialLinks.TELEGRAM} target="_blank">
         <svg id="icon-telegram" className={s.iconSocial}>
           <use href={sprite + '#icon-telegram'}></use>
         </svg>
       </a>
-      <a href={SOCIAL_NETWORKS.VIBER} target="_blank">
+      <a href={socialLinks.VIBER} target="_blank">
         <svg id="icon-viber" className={s.iconSocial}>
           <use href={sprite + '#icon-viber'}></use>
         </svg>
       </a>
-      <a href={SOCIAL_NETWORKS.FACEBOOK} target="_blank">
+      <a href={socialLinks.FACEBOOK} target="_blank">
         <svg id="icon-facebook" className={s.iconSocial}>
           <use href={sprite + '#icon-facebook'}></use>
         </svg>
